080e6fffe9e476bf1e2531aaa08330beaae4a163
Tyler Hall <tylerwhall@gmail.com>
2015-04-14 23:30:37 -0400

    Register a regulator

--- configfs-regulator.h
+++ configfs-regulator.h
@@ -2,12 +2,16 @@
 #define __CONFIGFS_REGULATOR_H__
 
 #include <linux/platform_device.h>
+#include <linux/configfs.h>
+#include <linux/regulator/driver.h>
 
 extern struct platform_driver configfs_regulator_driver;
 
 struct user_regulator {
 	struct platform_device pdev;
 	struct config_item config_item;
+	struct regulator_desc rdesc;
+	struct regulator_dev *rdev;
 };
 
 #endif
--- regulator.c
+++ regulator.c
@@ -14,14 +14,52 @@
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/regulator/driver.h>
+
+#include "configfs-regulator.h"
+
+static inline struct user_regulator *pdev_to_user_regulator(struct platform_device *pdev)
+{
+	return container_of(pdev, struct user_regulator, pdev);
+}
+
+static inline struct user_regulator *dev_to_user_regulator(struct device *dev)
+{
+	return pdev_to_user_regulator(to_platform_device(dev));
+}
+
+static const struct regulator_ops configfs_reg_ops = {
+};
 
 static int configfs_regulator_probe(struct platform_device *pdev)
 {
+	struct user_regulator *ureg = pdev_to_user_regulator(pdev);
+	struct regulator_config config = {
+		.dev = &pdev->dev,
+	};
+
+	ureg->rdesc.name = config_item_name(&ureg->config_item);
+	ureg->rdesc.id = -1;
+	ureg->rdesc.type = REGULATOR_VOLTAGE;
+	ureg->rdesc.owner = THIS_MODULE;
+	ureg->rdesc.ops = &configfs_reg_ops;
+
+	ureg->rdev = regulator_register(&ureg->rdesc, &config);
+	if (IS_ERR(ureg->rdev)) {
+		int rc = PTR_ERR(ureg->rdev);
+		dev_err(&pdev->dev, "Failed to register regulator: %d\n", rc);
+		return rc;
+	}
+
 	return 0;
 }
 
 static int configfs_regulator_remove(struct platform_device *pdev)
 {
+	struct user_regulator *ureg = pdev_to_user_regulator(pdev);
+
+	regulator_unregister(ureg->rdev);
+
 	return 0;
 }
 
